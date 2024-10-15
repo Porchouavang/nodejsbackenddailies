e5c40508971f16b7ad3632886438b550be55f3f9
pcvks <kangserpobtsuasvaaj@gmail.com>
2024-10-14 19:56:21 -0700

    update db-name

--- dbConnection.js
+++ dbConnection.js
@@ -6,10 +6,10 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_NAME
 });
 
-console.log('Database:', process.env.DB_DATABASE);
+console.log('Database:', process.env.DB_NAME);
 connection.connect((err) => {
     if (err) {
         console.error("Error connecting to mysql", err);
